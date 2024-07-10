c1fa330398e1379fb43a964091567e453ba92146
Amanpathak504 <131956062+Amanpathak504@users.noreply.github.com>
2024-07-10 20:46:48 +0530

    kidscreativehub

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'head.php';?>
+<body>
+	<?php include 'topbar.php';?>
+	<?php include 'header.php';?>
+	<?php include 'hero.php';?>
+  <main id="main">
+ <?php include 'featured.php';?>
+ <?php include 'about.php';?>
+<?php include 'services.php';?>
+<?php include 'clients.php';?>
+  </main><!-- End #main -->
+
+  <?php include 'footer.php';?>
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <!-- Vendor JS Files -->
+  <?php include 'js.php';?>
+
+</body>
+
+</html>
